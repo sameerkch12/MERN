18c54db06faefe2678e7fe3d3b3bc467a42fecbb
Sameer Choudhary <62991891+sameerkch12@users.noreply.github.com>
2023-07-31 05:39:59 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const mongoDB = require("./db")
+const express = require('express');
+const app = express();
+const port = 5000;
+const mongoDB = require("./db");
+const cors = require("cors");
 
 mongoDB();
 
-//ye banana he padt hai jab frontend port 3000 se backend port 5000 pe data accept krna hota haii to
+// Set up CORS to allow requests from your updated frontend URL
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://sameer-mern-app.netlify.app/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
@@ -15,17 +16,24 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json())
+// More CORS setup
+app.use(
+  cors({
+    origin: "https://sameer-mern-app.netlify.app/",
+    methods: ["GET", "POST"],
+  })
+);
+
+app.use(express.json());
 app.use('/api', require("./Routes/CreateUser"));
 app.use('/api', require("./Routes/DisplayData"));
 app.use('/api', require("./Routes/OrderData"));
 
-
-//nomal express start
+// Default route
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
